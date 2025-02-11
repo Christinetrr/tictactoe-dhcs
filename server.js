2e5a87ebbc892e9ff538c844675523f7c26a2532
Glitch (iced-axiomatic-watchmaker) <none>
2025-02-11 00:49:14 +0000

    🍍🌍 Checkpoint ./server.js:74831287/680

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-// This is the server code and it is deliberately very simple. You could
+// This is the server code. Its job is to serve static web pages, 
+// and to receive and transmit websocket messages. It
+// 
+// You could
 
 
 // We're using two libraries. 
@@ -15,21 +18,36 @@ const socketIO = require("socket.io");
 // you run on your own computer), the "||" will mean it resolves to 3000 instead.
 const PORT = process.env.PORT || 3000;
 
-// Initiate an Express server
+// Initiate an Express server. Tell it to serve the contents of the "public"
+// folder as static files, and listen on the port we set above.
 const server = express()
 	.use(express.static('public'))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-
+// Initiate a socket listener.
 const io = socketIO(server);
+
+// Whenever the socket gets a new message from a client, do the following...
 io.on("connection", function(socket) {
+  //   if the message is "disconnect" (meaning the client has disconnected)
 	socket.on("disconnect", function() {
+    // nothing here for now, but you could imagine 
+    // emitting a message to tell others that someone disconnected...
 	});
-	socket.on("message", function(msg) { // leaving this in case I want to use it for other messages
+  
+  
+	socket.on("message", function(msg) { 
+      //  log it to the server logs
 	    console.log(msg);
+      //  "emit" means to send back to the same client that sent this message we just got
+      socket.emit("ok");
 	});
+  
+  //   when you get a message of type "paint"...
 	socket.on("paint", function(data){
+    //  log it to the server logs
 		console.log(data);
+    // and "broadcast" it -- this means to emit the message to everyone *except* who the message came from
 		socket.broadcast.emit("paint", data); // "broadcast" flag means "emit to everyone *else*"
 	});
 });
